cc802dfdef5309bf5442dbec926c0e4fcb849342
SrRiuz <mateo.jimenez@alternova.com>
2023-03-03 22:35:37 -0500

    Add api calendar urls

--- modules/calendar/filter.py
+++ modules/calendar/filter.py
@@ -9,14 +9,18 @@ class Filter:
         self.__pandas = pandas.DataFrame(data)
         self.__data = data
 
-    def filter(self, week_days:List[Dict],
+    def filter(self, week_days:List[Dict], all:bool,
                day:Optional[str] = None) -> List[Dict]:
         """ Filter the data for days of the weeks """
 
         if not self.__data:
-            return None
+            return []
+        
+        if all:
+            return self.__pandas.to_dict(
+                orient="records")
 
-        if day or day != "today":
+        if day:
             self.__pandas = self.__pandas\
                 [self.__pandas["day"] == day]
             return self.__pandas.to_dict(
--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 # Libs
 from fastapi import FastAPI
 
+# Middlewares
+from middlewares.token import token_middleware
+
 app = FastAPI()
+app.middleware("http")(token_middleware)
+
 
 # Views
 from modules.auth.views import *
-
+from modules.calendar.views import *
