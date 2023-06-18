f096c4e87546d2257128a429e32718dff4be57b3
Mateo Jimenez <sirvegas21@gmail.com>
2023-06-18 16:56:46 -0500

    Add location

--- libs/db.py
+++ libs/db.py
@@ -1,11 +1,12 @@
 # Libs
 import pymongo
 from pymongo.database import Database
+from settings import DATABASE_HOST, DATABASE_PORT
 
 
 class Database:
 
-    def get_conection(self) -> Database:
+    def get_conection(self) -> (Database):
         """ Get conection of mongo database """
-        client = pymongo.MongoClient("localhost", 27017)
+        client = pymongo.MongoClient(DATABASE_HOST, DATABASE_PORT)
         return client.test
